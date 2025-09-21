8e80df53ccad56d839032f13ec84d63bbd634ce6
Tanuj-02 <tanujcode02@gmail.com>
2025-09-21 10:56:17 +0530

    Bg fixed

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -4,7 +4,7 @@ const Background = ({ children }) => {
   return (
     <div className="relative min-h-screen bg-zinc-900 overflow-hidden">
       <div className="absolute inset-0 bg-purple-500/10 blur-2xl"></div>
-      <div className="absolute inset-0 bg-[url('https://www.transparenttextures.com/patterns/cubes.png')] opacity-5"></div>
+      <div className="absolute inset-0 bg-[url('https://www.transparenttextures.com/patterns/cubes.png')] opacity-25"></div>
       <div className="relative z-10">{children}</div>
     </div>
   );
