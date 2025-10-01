44268a408ed3f07029f2bd9419c50e7985450f0b
Tanuj-02 <tanujcode02@gmail.com>
2025-10-01 16:03:22 +0530

    google and vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
 
   return (
@@ -22,6 +22,7 @@ function App() {
       <Contact/>
       <Footer/>
     </div>
+    <Analytics/>
     </Background>
   )
 }
